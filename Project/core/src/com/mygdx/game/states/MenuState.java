9cbc23e2e87c68146238fc523ea05a3a3391ba44
lisamso <lisasoem@live.no>
2018-03-11 23:42:16 +0100

    Created the main menu.

--- Project/core/src/com/mygdx/game/states/MenuState.java
+++ Project/core/src/com/mygdx/game/states/MenuState.java
@@ -14,13 +14,36 @@ public class MenuState extends State {
         super();
         ImageComponent logo = new ImageComponent(new Texture("logo.png"));
         logo.setPosition((GdxGame.WIDTH-logo.getWidth())/2, (GdxGame.HEIGHT - logo.getHeight())*7/8);
-        Button button = new Button(200, 60);
-        button.setText("Hello world");
-        button.setPosition((GdxGame.WIDTH - button.getWidth())/2, (GdxGame.HEIGHT - button.getHeight())*3/4 );
-        button.setBorderWidth(5);
-        button.setFontColor(Color.BLUE);
-        addComponent(button);
+
+        Button playButton = new Button(200, 40);
+        playButton.setText("Play game");
+        playButton.setPosition((GdxGame.WIDTH - playButton.getWidth())/2, (GdxGame.HEIGHT - playButton.getHeight())*3/5 );
+        playButton.setBorderWidth(2);
+        playButton.setFontColor(Color.WHITE);
+        playButton.setBackgroundColor(Color.NAVY);
+        playButton.setBorderColor(Color.BLUE);
+
+        Button settingsButton = new Button(200, 40);
+        settingsButton.setText("Settings");
+        settingsButton.setPosition((GdxGame.WIDTH - settingsButton.getWidth())/2, (GdxGame.HEIGHT - settingsButton.getHeight())*2/5 );
+        settingsButton.setBorderWidth(2);
+        settingsButton.setFontColor(Color.WHITE);
+        settingsButton.setBackgroundColor(Color.NAVY);
+        settingsButton.setBorderColor(Color.BLUE);
+
+        Button quitButton = new Button(200, 40);
+        quitButton.setText("Quit");
+        quitButton.setPosition((GdxGame.WIDTH - quitButton.getWidth())/2, (GdxGame.HEIGHT - quitButton.getHeight())*1/5 );
+        quitButton.setBorderWidth(2);
+        quitButton.setFontColor(Color.WHITE);
+        quitButton.setBackgroundColor(Color.NAVY);
+        quitButton.setBorderColor(Color.BLUE);
+
         addComponent(logo);
+        addComponent(playButton);
+        addComponent(settingsButton);
+        addComponent(quitButton);
+
     }
 
     @Override
