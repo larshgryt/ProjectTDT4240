6ca87b4af05bc964c4bab9350ae4d97be316b64a
lisamso <lisasoem@live.no>
2018-03-11 20:59:34 +0100

    Created the stages class and added some abstract mothods to component.

--- Project/core/src/com/mygdx/game/components/Component.java
+++ Project/core/src/com/mygdx/game/components/Component.java
@@ -1,4 +1,9 @@
 package com.mygdx.game.components;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 public abstract class Component {
+    public abstract void update(float dt);
+    public abstract void render(SpriteBatch sb);
+    public abstract void dispose();
 }
--- Project/core/src/com/mygdx/game/states/State.java
+++ Project/core/src/com/mygdx/game/states/State.java
@@ -1,20 +1,70 @@
 package com.mygdx.game.states;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.components.Component;
 
-/**
- * Created by agava on 08.03.2018.
- */
+import java.util.ArrayList;
 
 public abstract class State {
-    protected GameStateManager gsm;
 
-    public State(GameStateManager gsm){
-        this.gsm = gsm;
+    private ArrayList<Component> components;
+
+    public State(){
+        components = new ArrayList<Component>();
     }
 
-    public abstract void handleInput();
-    public abstract void update(float dt);
-    public abstract void render(SpriteBatch sb);
-    public abstract void dispose();
+    protected ArrayList<Component> getAllComponents(){
+        return components;
+    }
+    protected void addComponent(Component component){
+        components.add(component);
+    }
+    protected boolean removeComponent(Component component){
+        for(Component c: components){
+            if(component.equals(c)){
+                components.remove(c);
+                return true;
+            }
+        }
+        return false;
+    }
+    protected Component removeComponent(int index){
+        if(index < 0 || index >= components.size()){
+            throw new IndexOutOfBoundsException();
+        }
+        else{
+            return components.remove(index);
+        }
+    }
+
+    protected Component getComponent(int index){
+        if(index < 0 || index >= components.size()){
+            throw new IndexOutOfBoundsException();
+        }
+        else{
+            return components.get(index);
+        }
+    }
+    protected int indexOfComponent(Component component){
+        return components.indexOf(component);
+    }
+
+    abstract void handleInput();
+
+    public void update(float dt){
+        for(Component component: components){
+            component.update(dt);
+        }
+    }
+    public void render(SpriteBatch sb){
+        for(Component component: components){
+            component.render(sb);
+        }
+    }
+    public void dispose(){
+        for(Component component: components){
+            component.dispose();
+        }
+    }
+
 }
