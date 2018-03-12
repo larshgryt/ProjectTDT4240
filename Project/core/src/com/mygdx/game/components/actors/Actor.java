518f2b9f76e3b2b7c4f21256183be4c5834ca7ad
lisamso <lisasoem@live.no>
2018-03-12 16:58:26 +0100

    Moved some components around, and added some methods to actor.

--- Project/core/src/com/mygdx/game/components/actors/Actor.java
+++ Project/core/src/com/mygdx/game/components/actors/Actor.java
@@ -2,6 +2,7 @@ package com.mygdx.game.components.actors;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
+import com.mygdx.game.components.CollisionBox;
 import com.mygdx.game.components.Component;
 import com.mygdx.game.sprites.Sprite;
 
@@ -10,14 +11,48 @@ public abstract class Actor extends Component {
     private Vector3 velocity;
     private Vector3 acceleration;
     private Sprite sprite;
+    private CollisionBox collisionBox;
 
     public Actor(){
         super();
         velocity = new Vector3(0, 0, 0);
         acceleration = new Vector3(0, 0,0);
         sprite = null;
+        collisionBox = new CollisionBox(CollisionBox.CollisionMode.NEVER,
+                CollisionBox.CollisionShape.RECTANGULAR, 0, 0);
     }
 
+    public Vector3 getVelocity() {
+        return velocity;
+    }
+
+    public void setVelocity(Vector3 velocity) {
+        this.velocity = velocity;
+    }
+
+    public Vector3 getAcceleration() {
+        return acceleration;
+    }
+
+    public void setAcceleration(Vector3 acceleration) {
+        this.acceleration = acceleration;
+    }
+
+    public Sprite getSprite() {
+        return sprite;
+    }
+
+    public void setSprite(Sprite sprite) {
+        this.sprite = sprite;
+    }
+
+    public CollisionBox getCollisionBox() {
+        return collisionBox;
+    }
+
+    public void setCollisionBox(CollisionBox collisionBox) {
+        this.collisionBox = collisionBox;
+    }
 
     @Override
     public void update(float dt) {
