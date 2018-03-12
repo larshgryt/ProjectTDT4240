7787cd18428719ac8069b052eeed72196689b872
An Nguyen <An Nguyen>
2018-03-12 15:54:47 +0100

    Added background music

--- Project/core/src/com/mygdx/game/audio/AudioService.java
+++ Project/core/src/com/mygdx/game/audio/AudioService.java
@@ -1,4 +1,27 @@
 package com.mygdx.game.audio;
 
-public class AudioService {
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+
+public class AudioService extends ApplicationAdapter{
+
+    Sound bgMusic;
+
+    @Override
+    public void create() {
+        bgMusic = Gdx.audio.newSound(Gdx.files.internal("background.mp3"));
+        long id = bgMusic.loop();
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void dispose() {
+        bgMusic.dispose();
+    }
+
 }
