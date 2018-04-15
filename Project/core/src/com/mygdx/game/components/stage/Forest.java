720273d515fbe1e315206203222cc49ebfcdff11
An Nguyen <An Nguyen>
2018-04-15 16:52:51 +0200

    Made class for repeating tiles

--- Project/core/src/com/mygdx/game/components/stage/Forest.java
+++ Project/core/src/com/mygdx/game/components/stage/Forest.java
@@ -1,11 +1,7 @@
 package com.mygdx.game.components.stage;
 
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.utils.TiledDrawable;
-import com.mygdx.game.components.stage.stagecomponents.Grass;
-import com.mygdx.game.components.stage.stagecomponents.StageComponent;
+import com.mygdx.game.components.stage.stagecomponents.Tile;
 
 /**
  * Created by An on 15.04.2018.
@@ -13,18 +9,11 @@ import com.mygdx.game.components.stage.stagecomponents.StageComponent;
 
 public class Forest extends Stage {
 
-    //StageComponent ground;
-
     public Forest(){
         super(1000, 400);
         setBackgroundTexture(new Texture("sky.jpg"));
 
-        TiledDrawable ground = new TiledDrawable(
-                new TextureRegion(
-                new Texture("grass.png")));
-        StageComponent groundComponent = new StageComponent(
-                ground, (int) width, (int) ground.getMinHeight());
-        groundComponent.setCollidable(true);
-        addStageComponent(groundComponent);
+        Texture grassTile = new Texture("grass.png");
+        addStageComponent(new Tile(grassTile, (int) width, grassTile.getHeight()));
     }
 }
