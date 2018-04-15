f99f6963b92ead02cbc3f7326e2306335819af42
An Nguyen <An Nguyen>
2018-04-15 21:31:38 +0200

    Changed Forest background and added tree

--- Project/core/src/com/mygdx/game/components/stage/Forest.java
+++ Project/core/src/com/mygdx/game/components/stage/Forest.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.mygdx.game.components.stage.stagecomponents.GrassPlatform;
 import com.mygdx.game.components.stage.stagecomponents.StageComponent;
 import com.mygdx.game.components.stage.stagecomponents.Tile;
+import com.mygdx.game.components.stage.stagecomponents.Tree;
 
 /**
  * Created by An on 15.04.2018.
@@ -13,12 +14,12 @@ public class Forest extends Stage {
 
     public Forest(){
         super(1000, 400);
-        setBackgroundTexture(new Texture("sky.jpg"));
+        setBackgroundTexture(new Texture("forest.png"));
 
         Texture grassTile = new Texture("grass.png");
-        StageComponent grassTileComponent = new Tile(grassTile, (int) width, grassTile.getHeight());
-        grassTileComponent.setFriction(0.45f);
-        addStageComponent(grassTileComponent);
+        StageComponent ground = new Tile(grassTile, (int) width, grassTile.getHeight());
+        ground.setFriction(0.45f);
+        addStageComponent(ground);
 
         GrassPlatform platform1 = new GrassPlatform();
         GrassPlatform platform2 = new GrassPlatform();
@@ -26,5 +27,9 @@ public class Forest extends Stage {
         platform2.setPosition(450, 100);
         addStageComponent(platform1);
         addStageComponent(platform2);
+
+        Tree tree = new Tree();
+        tree.setPosition(5, ground.getHeight());
+        addStageComponent(tree);
     }
 }
