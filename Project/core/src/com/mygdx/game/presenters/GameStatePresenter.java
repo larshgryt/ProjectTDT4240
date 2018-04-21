dd34aa8276769cb3c7c95dec3ebf9a08249a39d7
An Nguyen <An Nguyen>
2018-04-21 18:18:26 +0200

    Added ActionListener for click on player

--- Project/core/src/com/mygdx/game/presenters/GameStatePresenter.java
+++ Project/core/src/com/mygdx/game/presenters/GameStatePresenter.java
@@ -2,11 +2,14 @@ package com.mygdx.game.presenters;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.mygdx.game.GdxGame;
+import com.mygdx.game.components.actors.PlayerActor;
 import com.mygdx.game.components.menucomponents.Grid;
 import com.mygdx.game.components.menucomponents.ImageButton;
+import com.mygdx.game.handlers.GameHandler;
 import com.mygdx.game.handlers.input.InputEvent;
 import com.mygdx.game.handlers.input.InputHandler;
 import com.mygdx.game.states.GameState;
+import com.mygdx.game.states.GameStateManager;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -28,7 +31,7 @@ public class GameStatePresenter extends Presenter {
         ImageButton menuButton = new ImageButton(new Texture("menuButton.png"), "menuButton");
         menuButton.setPosition(GdxGame.WIDTH-menuButton.getWidth()-5,GdxGame.HEIGHT-5);
 
-        //Movement control buttons
+        // Movement control buttons
         ImageButton rightButton = new ImageButton(new Texture("right-button.png"), "rightButton");
         ImageButton leftButton = new ImageButton(new Texture("left-button.png"), "leftButton");
         rightButton.setPosition(GdxGame.WIDTH-rightButton.getWidth(), (GdxGame.HEIGHT-leftButton.getHeight())/2);
@@ -53,10 +56,15 @@ public class GameStatePresenter extends Presenter {
             }
         });
 
-
         addComponent(rightButton);
         addComponent(leftButton);
         addComponent(menuButton);
         addComponent(weaponButton);
+
+        if(GameStateManager.getInstance().getActiveState() instanceof GameState){
+            GameState gameState = (GameState)GameStateManager.getInstance().getActiveState();
+            GameHandler gameHandler = gameState.getGameHandler();
+            gameHandler.getActivePlayer();
+        }
     }
 }
