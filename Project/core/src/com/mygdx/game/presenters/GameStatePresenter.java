0b64d5b83b54de53b983f1cc8beb0dd341756c5e
An Nguyen <An Nguyen>
2018-04-22 09:24:43 +0200

    Cleaned up GameStatePresenter

--- Project/core/src/com/mygdx/game/presenters/GameStatePresenter.java
+++ Project/core/src/com/mygdx/game/presenters/GameStatePresenter.java
@@ -8,63 +8,61 @@ import com.mygdx.game.components.menucomponents.ImageButton;
 import com.mygdx.game.handlers.GameHandler;
 import com.mygdx.game.handlers.input.InputEvent;
 import com.mygdx.game.handlers.input.InputHandler;
+import com.mygdx.game.listeners.EventListener;
 import com.mygdx.game.states.GameState;
 import com.mygdx.game.states.GameStateManager;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.EventObject;
 import java.util.List;
 
 public class GameStatePresenter extends Presenter {
 
-    private List<ImageButton> weaponTextures = new ArrayList<ImageButton>();
-    private Grid weaponGrid;
-    private ImageButton weaponButton;
-
     public GameStatePresenter(){
         super();
-        weaponButton = new ImageButton(new Texture("bazooka_temporary.png"), "bazooka");
-        weaponButton.setPosition(5, (GdxGame.HEIGHT - weaponButton.getHeight()));
+        addComponent(new RightButton());
+        addComponent(new LeftButton());
+        addComponent(new WeaponButton());
+    }
 
+    private class RightButton extends ImageButton{
+        public RightButton(){
+            super(new Texture("right-button.png"), "rightButton");
+            setPosition(GdxGame.WIDTH-getWidth(), (GdxGame.HEIGHT-getHeight())/2);
+            addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    InputHandler.getInstance().processInputEvent(new InputEvent("right"));
+                }
+            });
+        }
+    }
 
-        ImageButton menuButton = new ImageButton(new Texture("menuButton.png"), "menuButton");
-        menuButton.setPosition(GdxGame.WIDTH-menuButton.getWidth()-5,GdxGame.HEIGHT-5);
+    private class LeftButton extends ImageButton{
+        public LeftButton(){
+            super(new Texture("left-button.png"), "leftButton");
+            setPosition(0, (GdxGame.HEIGHT-getHeight())/2);
+            addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    InputHandler.getInstance().processInputEvent(new InputEvent("left"));
+                }
+            });
+        }
+    }
 
-        // Movement control buttons
-        ImageButton rightButton = new ImageButton(new Texture("right-button.png"), "rightButton");
-        ImageButton leftButton = new ImageButton(new Texture("left-button.png"), "leftButton");
-        rightButton.setPosition(GdxGame.WIDTH-rightButton.getWidth(), (GdxGame.HEIGHT-leftButton.getHeight())/2);
-        leftButton.setPosition(0, (GdxGame.HEIGHT-leftButton.getHeight())/2);
-
-        rightButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                InputHandler.getInstance().processInputEvent(new InputEvent("right"));
-            }
-        });
-        leftButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                InputHandler.getInstance().processInputEvent(new InputEvent("left"));
-            }
-        });
-        weaponButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                InputHandler.getInstance().processInputEvent(new InputEvent("useWeapon"));
-            }
-        });
-
-        addComponent(rightButton);
-        addComponent(leftButton);
-        addComponent(menuButton);
-        addComponent(weaponButton);
-
-        if(GameStateManager.getInstance().getActiveState() instanceof GameState){
-            GameState gameState = (GameState)GameStateManager.getInstance().getActiveState();
-            GameHandler gameHandler = gameState.getGameHandler();
-            gameHandler.getActivePlayer();
+    private class WeaponButton extends ImageButton{
+        public WeaponButton(){
+            super(new Texture("bazooka_temporary.png"), "weapon");
+            setPosition(5, (GdxGame.HEIGHT - getHeight()));
+            addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    InputHandler.getInstance().processInputEvent(new InputEvent("useWeapon"));
+                }
+            });
         }
     }
 }
